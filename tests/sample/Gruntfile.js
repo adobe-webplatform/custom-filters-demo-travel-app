0751bb21563d7f33726a411c937ec3be1b371ad3
Alexandru Chiculita <achicu@adobe.com>
2013-03-24 11:02:41 -0700

    adding grunt file

---
+++ tests/sample/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      compile: {
+        options: grunt.file.readJSON('build.json')
+      }
+    },
+    concat: {
+      all: {
+        src: [
+          "../../third-party/prod/jquery-1.9.1.min.js",
+          "../../third-party/prod/prefixfree.min.js",
+          "../../third-party/dev/prefixfree.jquery.js",
+          "../../third-party/prod/underscore-min.js",
+          "../../third-party/prod/underscore.string.min.js",
+          "../../third-party/prod/sylvester.js",
+          "../../third-party/prod/backbone-min.js",
+          "../../third-party/prod/require.js",
+          "dist/main.min"
+        ],
+        dest: 'dist/all.min.js',
+        options: {
+          banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['requirejs', 'concat:all']);
+
+};
